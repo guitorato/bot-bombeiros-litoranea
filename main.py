a289dc7af14f7a4045f838e593b9e6e3238eca8c
Torato <guii.torato@gmail.com>
2026-03-12 15:43:29 -0300

    ajuste no main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import os
 import asyncio
+import config
 
 intents = discord.Intents.default()
 intents.members = True
@@ -12,24 +13,19 @@ bot = commands.Bot(
     intents=intents
 )
 
-
-# ---------------------------------
-# QUANDO O BOT INICIAR
-# ---------------------------------
 @bot.event
 async def on_ready():
     print(f"🚒 Bot online: {bot.user}")
 
+    guild = discord.Object(id=config.GUILD_ID)
+
     try:
-        synced = await bot.tree.sync()
+        synced = await bot.tree.sync(guild=guild)
         print(f"✅ Slash commands sincronizados: {len(synced)}")
     except Exception as e:
-        print(f"Erro ao sincronizar comandos: {e}")
+        print(f"Erro ao sincronizar: {e}")
 
 
-# ---------------------------------
-# CARREGAR COGS AUTOMATICAMENTE
-# ---------------------------------
 async def load_cogs():
     for file in os.listdir("./cogs"):
         if file.endswith(".py"):
@@ -37,9 +33,6 @@ async def load_cogs():
             print(f"Cog carregada: {file}")
 
 
-# ---------------------------------
-# MAIN
-# ---------------------------------
 async def main():
     async with bot:
         await load_cogs()
